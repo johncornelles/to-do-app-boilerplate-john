1ba8f44afe4493cbd7a95885326fa2097ef1b8cc
johncornelles <john.cornellews@kalvium.community>
2023-10-05 11:45:30 +0530

    victory viking

---
+++ app.js
@@ -0,0 +1,35 @@
+const inputs = document.getElementById("input")
+const add = document.querySelector("#button")
+const ul = document.querySelector("#todolist")
+
+
+add.onclick = () =>{
+    var li = document.createElement("li")
+    li.textContent = inputs.value;
+    ul.appendChild(li)
+    inputs.value = ""
+    var a = document.createElement("a")
+    a.textContent = "\u00d7"
+    li.append(a)
+    var span = document.createElement("span")
+    span.textContent = " |Edit"
+    a.append(span)
+}
+
+ul.addEventListener("click", (e) => {
+    if (e.target.tagName == "A"){
+        e.target.parentElement.remove();
+    }
+    if (e.target.tagName == "SPAN") {
+        editItem(e);
+      }
+})
+
+function editItem(e) {
+    let li = e.target.parentElement.parentElement;
+    let current = li.firstChild.textContent;
+    let updated = prompt("Enter new task", current);
+    if (updated != null && updated != "") {
+      li.firstChild.textContent = updated;
+    }
+  }
